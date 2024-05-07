34a15dae5e7dbbbca7e1f1914a2fa92d1167c669
Яна <80539642+lulalend@users.noreply.github.com>
2024-05-07 10:12:10 +0300

    added interaction with keys

--- index.js
+++ index.js
@@ -26,3 +26,10 @@ const changeSlide = (direction) => {
 
 upBtn.addEventListener('click', () => changeSlide('up'));
 downBtn.addEventListener('click', () => changeSlide('down'));
+document.addEventListener('keydown', event => {
+    if (event.key === 'ArrowUp') {
+        changeSlide('up');
+    } else if (event.key === 'ArrowDown') {
+        changeSlide('down');
+    }
+});
